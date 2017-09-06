e624c86e78635c1e781fd1bf6eeae4ebeac8f3d9
Martin Roy <mroy@internap.com>
2017-09-06 15:58:02 -0400

    Support adding a dict in a new key
    Some apis have sections appearing with a param or just because they feel
    like it.
    
    Trying to add a dict in a new key was causing a problem when the key did
    not exist before hand.

--- jabstract/__init__.py
+++ jabstract/__init__.py
@@ -4,7 +4,7 @@ import copy
 def apply_kwargs(kwargs, default_kwargs):
     for k, v in kwargs.items():
         if isinstance(v, dict):
-            default_kwargs[k] = apply_kwargs(v, default_kwargs[k])
+            default_kwargs[k] = apply_kwargs(v, default_kwargs.get(k, {}))
         else:
             default_kwargs[k] = v
     return default_kwargs
